9a4c987bb6f1855fa7fa11cc9acb57090d994845
Ayesha Siddiqua <ayesha@linkstaff.co.jp>
2019-10-16 16:28:53 +0600

    view added to recycleview dinamically

--- app/src/main/java/com/ayeshaproject/recycleview/MyAdapter.java
+++ app/src/main/java/com/ayeshaproject/recycleview/MyAdapter.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -31,10 +34,24 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
         ListItem item = listItems.get(position);
         holder.name.setText(item.name);
         holder.add.setText(item.add);
+        holder.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(context,"hello",Toast.LENGTH_LONG).show();
+                TextView textView = new TextView(context);
+                textView.setText("I am added dynamically to the view");
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT
+                );
+                textView.setLayoutParams(params);
+                holder.lnr.addView(textView);
+            }
+        });
 
     }
 
@@ -47,12 +64,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
         public TextView name;
         public TextView add;
+        public Button btn;
+        public LinearLayout lnr;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             name = itemView.findViewById(R.id.name);
             add = itemView.findViewById(R.id.add);
+            btn = itemView.findViewById(R.id.btn);
+            lnr = itemView.findViewById(R.id.linear);
         }
     }
 }
